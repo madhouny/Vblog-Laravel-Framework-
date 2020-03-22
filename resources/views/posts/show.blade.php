399028b7cfa98f93487737789e5afdc79f504fbf
madhouny <younes.madhoun@gmail.com>
2020-03-23 00:41:55 +0100

    Ajout de deux bouton edit & delete sans avoir encore les implémenter

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,38 @@
 @endsection
 
 @section('content')
+<div class="row">
+    <div class="col-md-8">
+        <h1>{{$post->title}}</h1>
+        <p class="lead"> {{$post->body}}</p>
+    </div>
 
-<h1>{{$post->title}}</h1>
-    <p class="lead"> {{$post->body}}</p>
+    <div class="col-md-4">
+        <div class="card card-body bg-light">
+           <dl class="row">
+               <dt class="col-sm-6">Créé à:</dt>
+               <dd class="col-sm-6">{{ date('M j, Y H:i',strtotime($post->created_at)) }}</dd>
+           </dl>
+
+           <dl class="row">
+                <dt class="col-sm-6">Dernière mise à jour:</dt>
+                <dd class="col-sm-6">{{date('M j, Y H:i',strtotime($post->updated_at)) }}</dd>
+            </dl>
+            <hr>
+
+            <div class="row">
+                <div class="col-sm-6">
+
+                <a href="{{route('posts.edit', $post->id)}}" class="btn btn-primary btn-block">Modifier</a>
+                </div>
+
+                <div class="col-sm-6">
+                <a href="{{route('posts.destroy', $post->id)}}" class="btn btn-danger btn-block">Delete</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 @endsection
