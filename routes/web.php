e8e7a7fea24d47b458bc89d6c216f2b2dfd76629
madhouny <younes.madhoun@gmail.com>
2020-03-22 15:43:28 +0100

    creation du PostController & CRUD et Restful Routes

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/contact', 'PagesController@getContact');
-
 Route::get('/about', 'PagesController@getAbout');
-
-
 Route::get('/', 'PagesController@getIndex');
+Route::resource('posts','PostController');
